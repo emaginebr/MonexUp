ff07942f7b7707012f8216a321b1bbefcb52751c
rodrigo.carneiro <rodrigo.carneiro@benner.com.br>
2025-05-06 22:13:45 -0300

    Configure Deploy

--- Frontend/monexup-app/public/env.js
+++ Frontend/monexup-app/public/env.js
@@ -3,6 +3,7 @@ window.env = {
   "NODE_PATH": "./src",
   "PUBLIC_URL": "/",
   "SITE_BASENAME": "/",
-  "API_BASE_URL": "https://localhost:44374",
+  //"API_BASE_URL": "https://localhost:44374",
+  "API_BASE_URL": "https://monexup.com:8080",
   "PORT": "443"
 };
