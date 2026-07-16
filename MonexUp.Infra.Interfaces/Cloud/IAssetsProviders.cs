7b510fd28de151c80bdc7df442939dcd4c69199b
Rodrigo Landim <rodrigo@emagine.com.br>
2026-07-16 14:44:29 -0300

    fix: update deploy

--- MonexUp.Infra.Interfaces/Cloud/IAssetsProviders.cs
+++ MonexUp.Infra.Interfaces/Cloud/IAssetsProviders.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +8,6 @@ namespace Core.Domain.Cloud
 {
     public interface IAssetsProviders
     {
-        Bitmap GetBaseAssets(string baseFolder, string imgPath);
         Task<string> UploadFileToBlobAsync(string strFileName, byte[] fileData, string fileMimeType);
         Task DeleteFile(string strFileName);
     }
--- MonexUp.Infra/Repository/OrderRepository.cs
+++ MonexUp.Infra/Repository/OrderRepository.cs
@@ -7,7 +7,6 @@ using MonexUp.DTO.Order;
 using NoobsMuc.Coinmarketcap.Client;
 using System;
 using System.Collections.Generic;
-using System.Drawing.Printing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
